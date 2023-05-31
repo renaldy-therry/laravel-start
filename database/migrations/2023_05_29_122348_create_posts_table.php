3e0b7937dfa7b0be3b55eece45435eced8f2f9c3
Renaldy <renaldytherrydota@gmail.com>
2023-05-31 19:08:59 +0800

    third commit

--- database/migrations/2023_05_29_122348_create_posts_table.php
+++ database/migrations/2023_05_29_122348_create_posts_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('title');
+            $table->text('content');
         });
     }
 
